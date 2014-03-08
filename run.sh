fb9f9f97a6c4d8d80b030608fd15c022895c631f
Romuald du Song <romuald.dusong@deveryware.com>
2014-03-08 12:00:44 +0100

    upgrade to latest kibana (3 milestone 5)

--- run.sh
+++ run.sh
@@ -1,17 +1,29 @@
 #!/bin/bash
-ES_HOST=${ES_HOST:-window.location.hostname}
-ES_PORT=${ES_PORT:-9200}
+
+# check for link configuration via -link elasticsearch:es
+if [ -n "$ES_PORT_9200_TCP" ] ; then
+    ES_HOST="window.location.hostname"
+    ES_PORT="9200"
+else
+  ES_HOST=${ES_HOST:-window.location.hostname}
+  ES_PORT=${ES_PORT:-9200}
+fi
 
 cat << EOF > /usr/share/nginx/www/config.js
-var config = new Settings(
+define(['settings'],
+function (Settings) {
+  return new Settings(
 {
-  elasticsearch:    "http://$ES_HOST:$ES_PORT",
-  kibana_index:     "kibana-int",
-  modules:          ['histogram','map','pie','table','stringquery','sort',
-                    'timepicker','text','fields','hits','dashcontrol',
-                    'column','derivequeries','trends','bettermap'],
+  elasticsearch:    "http://"  + $ES_HOST + ":" + "$ES_PORT",
+  default_route:    '/dashboard/file/default.json',
+  kibana_index:     'kibana-int',
+  modules:          ['histogram','map','pie','table','filtering',
+                    'timepicker','text','hits',
+                    'column','trends','bettermap',
+                    'query', 'terms', 'stats','sparklines'],
   }
 );
+});
 EOF
 
 nginx -c /etc/nginx/nginx.conf
